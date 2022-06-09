9e700d2f8d94ba4296967a497b4ec8f3a3d54811
Fakhareddine SABBARI <fakhareddine.sabbari@tiempo-secure.com>
2022-06-09 13:54:59 +0200

    clean axi_dpi.sv

--- sources/verilog/axi_dpi.sv
+++ sources/verilog/axi_dpi.sv
@@ -50,7 +50,7 @@ import "DPI-C" context function int axi_server(
                       output int wdata_payload1,
                       output int wdata_payload2,
                       output int wdata_payload3,
-                      output bit  wdata_last,
+                      output bit wdata_last,
                       output bit rdata_ready,
                       input int rdata_payload0,
                       input int rdata_payload1,
@@ -68,17 +68,16 @@ import "DPI-C" context function int axi_server(
 parameter port = 7897;
 parameter blocking = 1;
 parameter word_size=16;
-reg [31:0]  addr,size,datacount;
-reg [31:0] wdata_payload0,wdata_payload1,wdata_payload2,wdata_payload3;
+reg  [31:0]  addr,size,datacount;
+reg  [31:0] wdata_payload0,wdata_payload1,wdata_payload2,wdata_payload3;
 wire [31:0] rdata_payload0,rdata_payload1,rdata_payload2,rdata_payload3;
-wire [1:0] rsp_payload;
-wire  rsp_valid;
-reg write;
-reg wdata_valid,wdata_last;
-reg rdata_ready;
-wire rdata_valid,rdata_last;
-wire wdata_ready;
-int value ;
+wire [1: 0] rsp_payload;
+wire        rsp_valid;
+reg         write;
+reg         wdata_valid,wdata_last;
+reg         rdata_ready;
+wire        rdata_valid,rdata_last;
+wire        wdata_ready;
 int return_value;
 always @(posedge clk)
 begin
@@ -111,31 +110,31 @@ begin
 end
 
  simpleAxi4Master aximaster(
-  .masterAxi_aw_valid(masterAxi_aw_valid),
-  .masterAxi_aw_ready(masterAxi_aw_ready),
-  .masterAxi_aw_payload_addr(masterAxi_aw_payload_addr),
-  .masterAxi_aw_payload_len(masterAxi_aw_payload_len),
-  .masterAxi_aw_payload_size(masterAxi_aw_payload_size),
-  .masterAxi_aw_payload_burst(masterAxi_aw_payload_burst),
-  .masterAxi_w_valid(masterAxi_w_valid),
-  .masterAxi_w_ready(masterAxi_w_ready),
-  .masterAxi_w_payload_data(masterAxi_w_payload_data),
-  .masterAxi_w_payload_strb(masterAxi_w_payload_strb),
-  .masterAxi_w_payload_last(masterAxi_w_payload_last),
-  .masterAxi_b_valid(masterAxi_b_valid),
-  .masterAxi_b_ready(masterAxi_b_ready),
-  .masterAxi_b_payload_resp(masterAxi_b_payload_resp),
-  .masterAxi_ar_valid(masterAxi_ar_valid),
-  .masterAxi_ar_ready(masterAxi_ar_ready),
-  .masterAxi_ar_payload_addr(masterAxi_ar_payload_addr),
-  .masterAxi_ar_payload_len(masterAxi_ar_payload_len),
-  .masterAxi_ar_payload_size(masterAxi_ar_payload_size),
-  .masterAxi_ar_payload_burst(masterAxi_ar_payload_burst),
-  .masterAxi_r_valid(masterAxi_r_valid),
-  .masterAxi_r_ready(masterAxi_r_ready),
-  .masterAxi_r_payload_data(masterAxi_r_payload_data),
-  .masterAxi_r_payload_resp(masterAxi_r_payload_resp),
-  .masterAxi_r_payload_last(masterAxi_r_payload_last),
+  .masterAxi_aw_valid            (masterAxi_aw_valid),
+  .masterAxi_aw_ready            (masterAxi_aw_ready),
+  .masterAxi_aw_payload_addr     (masterAxi_aw_payload_addr),
+  .masterAxi_aw_payload_len      (masterAxi_aw_payload_len),
+  .masterAxi_aw_payload_size     (masterAxi_aw_payload_size),
+  .masterAxi_aw_payload_burst    (masterAxi_aw_payload_burst),
+  .masterAxi_w_valid             (masterAxi_w_valid),
+  .masterAxi_w_ready             (masterAxi_w_ready),
+  .masterAxi_w_payload_data      (masterAxi_w_payload_data),
+  .masterAxi_w_payload_strb      (masterAxi_w_payload_strb),
+  .masterAxi_w_payload_last      (masterAxi_w_payload_last),
+  .masterAxi_b_valid             (masterAxi_b_valid),
+  .masterAxi_b_ready             (masterAxi_b_ready),
+  .masterAxi_b_payload_resp      (masterAxi_b_payload_resp),
+  .masterAxi_ar_valid            (masterAxi_ar_valid),
+  .masterAxi_ar_ready            (masterAxi_ar_ready),
+  .masterAxi_ar_payload_addr     (masterAxi_ar_payload_addr),
+  .masterAxi_ar_payload_len      (masterAxi_ar_payload_len),
+  .masterAxi_ar_payload_size     (masterAxi_ar_payload_size),
+  .masterAxi_ar_payload_burst    (masterAxi_ar_payload_burst),
+  .masterAxi_r_valid             (masterAxi_r_valid),
+  .masterAxi_r_ready             (masterAxi_r_ready),
+  .masterAxi_r_payload_data      (masterAxi_r_payload_data),
+  .masterAxi_r_payload_resp      (masterAxi_r_payload_resp),
+.masterAxi_r_payload_last        (masterAxi_r_payload_last),
   .tcpBus_addr                   (addr[31:0]), //i
   .tcpBus_wdata_valid            (wdata_valid), //i
   .tcpBus_wdata_ready            (wdata_ready), //o
@@ -148,8 +147,8 @@ end
   .tcpBus_rdata_payload_fragment ({rdata_payload3,rdata_payload2,rdata_payload1,rdata_payload0}), //o
   .tcpBus_rsp_valid              (rsp_valid), //o
   .tcpBus_rsp_payload            (rsp_payload[1:0]), //o
-  .clk(clk),
-  .reset(reset)
+  .clk                           (clk),
+  .reset                         (reset)
 );
 
 
